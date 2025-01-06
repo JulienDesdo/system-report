1afc0d94a83c8afae88699dc4e4ce7f5d0a5dcf0
JulienDesdo <frank.desdo@gmail.com>
2025-01-07 00:44:21 +0100

    Beginning Test on sudo command for writing log in /var/log/system_reports

--- src/system-report.sh
+++ src/system-report.sh
@@ -1,2 +1,20 @@
-#!/bin/sh
-echo "Hello world"
+#!/bin/bash
+
+log_dir="/var/log/system_reports"
+mkdir -p "$log_dir"
+chmod 755 "$log_dir"
+
+log_file="$log_dir/report_$(date +%Y-%m-%d_%H-%M-%S).log"
+
+echo "Rapport système - $(date)" > "$log_file"
+
+echo "Disk Usgae :" >> "$log_file"
+df -h >> "$log_file"
+
+echo "Memory usage :" >> "$log_file"
+free -h >> "$log_file"
+
+echo "System Operating time :" >> "$log_file"
+uptime >> "$log_file"
+
+echo "Rapport enregistré dans $log_dir"
